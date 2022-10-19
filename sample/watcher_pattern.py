f3fa80c80d79e5a2fd892ce7398504e26ac66ce6
AyanF <ayanfarooqui211@gmail.com>
2022-10-19 11:03:36 +0530

    Defined watcher task

--- sample/watcher_pattern.py
+++ sample/watcher_pattern.py
@@ -4,3 +4,7 @@ from airflow.decorators import task
 from airflow.exceptions import AirflowException
 from airflow.operators.bash import BashOperator
 from airflow.utils.trigger_rule import TriggerRule
+
+@task(trigger_rule=TriggerRule.ONE_FAILED, retries=0)
+def watcher():
+    raise AirflowException("Failing task because one or more upstream tasks failed.")
